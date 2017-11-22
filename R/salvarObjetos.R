d8bcdddce1151b76c3db9d0eee8f66d9301bd3e2
rucoma <ruben.coca@mail.com>
2017-11-23 00:35:03 +0100

    Added list.file

--- R/salvarObjetos.R
+++ R/salvarObjetos.R
@@ -1,13 +1,25 @@
 library(data.table)
 
-mes <- "201711"
+mes <- "201710"
+carpeta         <- "~/Dropbox/MOOCs/R/P48/"
+carpetaRObjects <- "~/Dropbox/MOOCs/R/P48/RObjects/"
 
-dt <- data.table(v1 = rnorm(n = 100, mean = 0, sd = 0),
-                 v2 = rnorm(n = 100, mean = 0, sd = 0),
-                 v3 = rnorm(n = 100, mean = 0, sd = 0))
+dt <- 
+  data.table(v1 = rnorm(n = 100, mean = 0, sd = 0),
+             v2 = rnorm(n = 100, mean = 0, sd = 0),
+             v3 = rnorm(n = 100, mean = 0, sd = 0))
 
 assign(paste0(deparse(substitute(dt)), mes), dt)
 
-save(list = ls(pattern = "2017"),
-     file = )
+save(list = ls(pattern = mes), # O en vez de mes poner p48Final
+     file = paste0(carpetaRObjects, mes, ".RData"))
 
+rm(list = ls())
+
+objetos <- 
+  list.files(path = "~/Dropbox/MOOCs/R/P48/RObjects", 
+             full.names = T, 
+             recursive = T,
+             pattern = "\\.RData")
+
+lapply(objetos, load, .GlobalEnv)
